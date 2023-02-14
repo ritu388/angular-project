81a20c09c999d8eb534949747ad23d926821bf25
Ritu1501 <ritu.p@mobinexttech.com>
2023-02-14 10:47:16 +0530

    sort table changes done

--- src/app/covid-page/covid-page.component.ts
+++ src/app/covid-page/covid-page.component.ts
@@ -16,6 +16,7 @@ export class CovidPageComponent implements OnInit {
   countPerEachPage = 10;
   countOfPages = 0;
   cPrev = -1;
+  asc = 0;
   constructor(public homeService: HomeService) { }
 
   ngOnInit(): void {
@@ -89,52 +90,89 @@ export class CovidPageComponent implements OnInit {
   }
 
   sortTable(n) {
-    console.log('enter in sortTable function')
-    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-      table = document.getElementById("table");
-      console.log('table', table);
-      switching = true;
-      dir = "asc"; 
-      while (switching) {
-        switching = false;
-        rows = table.rows;
-        console.log('rows', rows)
-        /*Loop through all table rows (except the
-        first, which contains table headers):*/
-        for (i = 1; i < (rows.length - 1); i++) {
-          //start by saying there should be no switching:
-          shouldSwitch = false;
-          x = rows[i].getElementsByTagName("td")[n];
-          console.log('x', x, i)
-          y = rows[i + 1].getElementsByTagName("td")[n];
-          console.log('y', y, i + 1)
-          if (dir == "asc") {
-            console.log('enter in asc condition', dir)
-            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-              shouldSwitch= true;
-              break;
-            }
-          } 
-          else if (dir == "desc") {
-            console.log('enter in desc condition', dir)
-            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-              shouldSwitch = true;
-              break;
-            }
-          }
-        }
-        if (shouldSwitch) {
-          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-          switching = true;
-          //Each time a switch is done, increase this count by 1:
-          switchcount ++;      
-        } 
-        else {
-          if (switchcount == 0 && dir == "asc") {
-            dir = "desc";
-            switching = true;
-          }
-        }
-      }
+    // console.log('enter in sortTable function')
+    // var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    //  let table = document.getElementById("table");
+    //   console.log('table', table);
+    //   switching = true;
+    //   dir = "asc"; 
+    //   while (switching) {
+    //     switching = false;
+    //     rows = table.rows;
+    //     console.log('rows', rows)
+    //     for (i = 1; i < (rows.length - 1); i++) {
+    //       shouldSwitch = false;
+    //       x = rows[i].getElementsByTagName("td")[n];
+    //       console.log('x', x, )
+    //       y = rows[i + 1].getElementsByTagName("td")[n];
+    //       console.log('y', y,)
+    //       if (dir == "asc") {
+    //         console.log('enter in asc condition', dir)
+    //         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+    //           console.log('ascending the number', x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase())
+    //           shouldSwitch= true;
+    //           break;
+    //         }
+    //       } 
+    //       else if (dir == "desc") {
+    //         console.log('enter in desc condition', dir)
+    //         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+    //           console.log('descending the number ', x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase())
+    //           shouldSwitch = true;
+    //           break;
+    //         }
+    //       }
+    //     }
+    //     if (shouldSwitch) {
+    //       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+    //       switching = true;
+    //       switchcount ++;      
+    //     } 
+    //     else {
+    //       if (switchcount == 0 && dir == "asc") {
+    //         dir = "desc";
+    //         switching = true;
+    //       }
+    //     }
+    //   }
+    this.sort_Table(document.getElementById("table"), n);
   }
+
+  sort_Table(table, col) {
+    if(this.asc == 2) { this.asc = -1;} else {this.asc = 2;}
+    let rows = table.tBodies[0].rows;
+    let rlen = rows.length-1;
+    let arr = new Array();
+    let i, j, cells, clen;
+    for(i = 0; i < rlen; i++) {
+      cells = rows[i].cells;
+      clen = cells.length;
+      arr[i] = new Array();
+      for(j = 0 ; j < clen; j++) {
+        arr[i][j] = cells[j].innerHTML; 
+      }
+    }
+    arr.sort((a, b) => {
+      var retval=0;
+      var col1 = a[col].toLowerCase().replace(',', '').replace('$', '').replace(' usd', '')
+      var col2 = b[col].toLowerCase().replace(',', '').replace('$', '').replace(' usd', '')
+      var fA=parseFloat(col1);
+      var fB=parseFloat(col2);
+      if(col1 != col2){
+        if((fA==col1) && (fB==col2) ){ retval=( fA > fB ) ? this.asc : -1*this.asc; } //numerical
+        else { retval=(col1 > col2) ? this.asc : -1*this.asc;}
+      }
+      return retval; 
+    });
+    for(var rowidx=0;rowidx<rlen;rowidx++){
+      for(var colidx=0;colidx<arr[rowidx].length;colidx++){ table.tBodies[0].rows[rowidx].cells[colidx].innerHTML=arr[rowidx][colidx]; }
+    }
+   let hdr = table.rows[0].cells[col];
+    if (this.asc == -1) {
+      hdr.html((hdr).html() + '<span class="sortorder">▲</span>');
+    } else {
+      hdr.html((hdr).html() + '<span class="sortorder">▼</span>');
+    }
+  } 
+  
 }
